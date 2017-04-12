e56fb56e7402d22ad47fc852976ddf4791ca6d24
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2017-04-12 15:31:40 +0300

    fix8

--- src/app/tasks/task-list/task-list.component.ts
+++ src/app/tasks/task-list/task-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from './../../rxjs-extensions';
+import { Subscription } from 'rxjs/Subscription';
 
 import { Task } from './../../models/task';
 import { TaskArrayService, TaskPromiseService, TaskObservableService } from './../';
